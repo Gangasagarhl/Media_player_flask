2dce5ab5dbf7c516ce9ec8013db39e2d32c1d801
HLG Sagar <55350412+Gangasagarhl@users.noreply.github.com>
2021-11-08 20:28:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask 
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return render_template('media.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
